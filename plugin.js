f6d513d27126681c5f78808d8e2f18ff05062dfa
vallushop-cmd <vallushop@gmail.com>
2026-01-16 21:59:47 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,41 +1,34 @@
 (function () {
     'use strict';
 
-    function init() {
-        // Удаляем старые неудачные кнопки из главного меню (если они там остались)
-        $('.menu__item[data-component="custom_yt"], .menu__item[data-component="custom_srv"]').remove();
-
-        // Создаем свое меню вместо стандартного окна выхода
-        Lampa.Exit.show = function () {
-            Lampa.Select.show({
-                title: 'Действие',
-                items: [
-                    {title: 'YouTube', action: 'yt'},
-                    {title: 'RuTube', action: 'rt'},
-                    {title: 'Очистить кэш', action: 'clr'},
-                    {title: 'Перезагрузка', action: 'rel'},
-                    {title: 'Выйти из приложения', action: 'exit'}
-                ],
-                onSelect: function (item) {
-                    if (item.action == 'yt') window.location.href = 'https://www.youtube.com';
-                    if (item.action == 'rt') window.location.href = 'https://rutube.ru';
-                    if (item.action == 'clr') { localStorage.clear(); window.location.reload(); }
-                    if (item.action == 'rel') window.location.reload();
-                    if (item.action == 'exit') {
-                        // Вызываем настоящий выход
-                        navigator.app ? navigator.app.exitApp() : window.close();
+    // Эта функция сработает только при нажатии на "Выход"
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type == 'ready') {
+            Lampa.Exit.show = function () {
+                Lampa.Select.show({
+                    title: 'Выход и Сервис',
+                    items: [
+                        {title: 'YouTube', action: 'yt'},
+                        {title: 'RuTube', action: 'rt'},
+                        {title: 'Очистить кэш', action: 'clr'},
+                        {title: 'Перезагрузка', action: 'rel'},
+                        {title: 'Выйти из Лампы', action: 'exit'}
+                    ],
+                    onSelect: function (item) {
+                        if (item.action == 'yt') window.location.href = 'https://www.youtube.com';
+                        if (item.action == 'rt') window.location.href = 'https://rutube.ru';
+                        if (item.action == 'clr') { localStorage.clear(); window.location.reload(); }
+                        if (item.action == 'rel') window.location.reload();
+                        if (item.action == 'exit') {
+                             if(window.appready) Lampa.Activity.out();
+                             else window.location.reload();
+                        }
+                    },
+                    onBack: function () {
+                        Lampa.Select.close();
                     }
-                },
-                onBack: function () {
-                    Lampa.Select.close();
-                }
-            });
-        };
-    }
-
-    // Ждем полной готовности Лампы
-    if (window.appready) init();
-    else Lampa.Listener.follow('app', function (e) {
-        if (e.type == 'ready') init();
+                });
+            };
+        }
     });
 })();

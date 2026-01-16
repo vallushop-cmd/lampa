7c88041db5819837a8e649852be5102a6cfb95b1
vallushop-cmd <vallushop@gmail.com>
2026-01-16 23:41:48 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,10 +1,23 @@
-(function() {
+(function () {
+
     if (!window.Lampa) return;
 
-    Lampa.Activity.push({
-        title: 'YouTube',
-        component: 'external',
-        icon: 'youtube',
-        url: 'tizen://launch?appId=111299001912'
+    // Ждём, когда всё загрузится
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type !== 'ready') return;
+
+        // Добавляем в меню YouTube
+        Lampa.MainMenu.add({
+            title: 'YouTube',
+            icon: 'youtube',
+            onClick: function () {
+                // для Tizen запускаем системный YouTube
+                Lampa.Platform.open('tizen://launch?appId=111299001912');
+            }
+        });
+
+        // Обновляем меню
+        Lampa.Controller.update('menu');
     });
+
 })();

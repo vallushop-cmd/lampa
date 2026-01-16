5ebf830be9a9bfa76521e6315f65896d6e14911f
vallushop-cmd <vallushop@gmail.com>
2026-01-16 23:25:09 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,20 +1,15 @@
 (function () {
-    'use strict';
+    if (!window.Lampa) return;
 
     Lampa.Listener.follow('app', function (e) {
-        if (e.type == 'ready') {
-            // Создаем пункт меню через системный метод Lampa
-            var item = {
+        if (e.type === 'ready') {
+            Lampa.MainMenu.add({
                 title: 'YouTube',
-                icon: '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M10 15l5.197-3L10 9v6z" fill="white"/><path d="M21.564 7.147c-.243-.907-.954-1.618-1.861-1.86-1.641-.44-8.203-.44-8.203-.44s-6.562 0-8.203.44c-.907.242-1.618.953-1.86 1.86-.44 1.64-.44 5.063-.44 5.063s0 3.423.44 5.063c.242.907.953 1.618 1.86 1.86 1.641.44 8.203.44 8.203.44s6.562 0 8.203-.44c.907-.242 1.618-.953 1.861-1.86.44-1.64.44-5.063.44-5.063s0-3.423-.44-5.063z" fill="white"/></svg>',
-                onSelect: function () {
-                    // Используем системный переход платформы
-                    window.location.href = 'https://www.youtube.com';
+                icon: 'youtube',
+                onClick: function () {
+                    Lampa.Platform.open('https://www.youtube.com');
                 }
-            };
-
-            // Добавляем строго в главное меню
-            Lampa.Menu.add(item);
+            });
         }
     });
 })();

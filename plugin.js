72213884d6fb70b0e60d03fb3d8ed075296b9cdc
vallushop-cmd <vallushop@gmail.com>
2026-01-16 23:47:56 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,16 +1,10 @@
 (function () {
     if (!window.Lampa) return;
 
-    // ждём, когда приложение готово
-    window.addEventListener('app:ready', function () {
-
-        // добавить пункт в меню
-        Lampa.MainMenu.add({
-            title: 'YouTube',
-            icon: 'youtube',
-            source: 'external',
-            url: 'tizen://launch?appId=111299001912'
-        });
-
+    Lampa.MainMenu.add({
+        title: 'YouTube',
+        icon: 'youtube',
+        source: 'external',
+        url: 'https://www.youtube.com/tv'
     });
 })();

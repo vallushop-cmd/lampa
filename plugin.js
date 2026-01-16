3e70c052aeb60c19f1525d87ff6a8a43eabd7c86
vallushop-cmd <vallushop@gmail.com>
2026-01-16 21:23:21 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,60 +1,70 @@
 (function () {
     'use strict';
 
-    function MyCustomPlugin() {
-        this.init = function () {
-            Lampa.Listener.follow('app', function (e) {
-                if (e.type == 'ready') {
-                    addCustomItems();
-                }
-            });
-        };
+    function startPlugin() {
+        console.log('Плагин: Запуск создания меню');
+        
+        // Функция добавления пунктов
+        function addItems() {
+            // Проверяем, не добавили ли мы уже пункты (чтобы не дублировать)
+            if ($('.menu__item[data-component="custom_yt"]').length > 0) return;
 
-        function addCustomItems() {
-            // 1. Кнопка YouTube
+            // Добавляем YouTube
             Lampa.Menu.add({
                 title: 'YouTube',
                 icon: '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" fill="white"/></svg>',
-                component: 'custom_youtube'
+                component: 'custom_yt',
+                page: true // Помогает отобразить в некоторых версиях
             });
 
-            // 2. Кнопка RuTube
-            Lampa.Menu.add({
-                title: 'RuTube',
-                icon: '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" fill="#f05022"/></svg>',
-                component: 'custom_rutube'
-            });
-
-            // 3. Пункт «Сервис»
+            // Добавляем Сервис
             Lampa.Menu.add({
                 title: 'Сервис',
                 icon: '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" stroke="white" stroke-width="2" fill="none"/></svg>',
-                component: 'custom_service'
-            });
-
-            Lampa.Component.add('custom_youtube', { render: function() { window.location.href = 'https://www.youtube.com'; return $('<div></div>'); }});
-            Lampa.Component.add('custom_rutube', { render: function() { window.location.href = 'https://rutube.ru'; return $('<div></div>'); }});
-            
-            Lampa.Component.add('custom_service', {
-                render: function() {
-                    Lampa.Select.show({
-                        title: 'Сервис',
-                        items: [
-                            {title: 'Перезагрузить', action: 'reload'},
-                            {title: 'Очистить кэш (Reset)', action: 'reset'}
-                        ],
-                        onSelect: function(item) {
-                            if (item.action == 'reload') window.location.reload();
-                            if (item.action == 'reset') { localStorage.clear(); window.location.reload(); }
-                        },
-                        onBack: function() { Lampa.Select.close(); Lampa.Controller.toggle('menu'); }
-                    });
-                    return $('<div></div>');
-                }
+                component: 'custom_srv',
+                page: true
             });
         }
+
+        // Регистрируем компоненты
+        Lampa.Component.add('custom_yt', {
+            render: function() {
+                window.location.href = 'https://www.youtube.com';
+                return $('<div></div>');
+            }
+        });
+
+        Lampa.Component.add('custom_srv', {
+            render: function() {
+                Lampa.Select.show({
+                    title: 'Сервис',
+                    items: [
+                        {title: 'Перезагрузить', action: 'rel'},
+                        {title: 'Очистить кэш', action: 'res'}
+                    ],
+                    onSelect: function(item) {
+                        if (item.action == 'rel') window.location.reload();
+                        if (item.action == 'res') { localStorage.clear(); window.location.reload(); }
+                    },
+                    onBack: function() { 
+                        Lampa.Select.close(); 
+                        Lampa.Controller.toggle('menu'); 
+                    }
+                });
+                return $('<div></div>');
+            }
+        });
+
+        // Пытаемся добавить пункты сразу
+        addItems();
+        
+        // И еще раз через 2 секунды на случай медленной загрузки меню
+        setTimeout(addItems, 2000);
     }
 
-    if (window.appready) new MyCustomPlugin().init();
-    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') new MyCustomPlugin().init(); });
+    // Слушаем полную готовность приложения
+    if (window.appready) startPlugin();
+    else Lampa.Listener.follow('app', function (e) {
+        if (e.type == 'ready') startPlugin();
+    });
 })();
